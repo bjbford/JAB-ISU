e4edd982d0859a6a90efab39486971f6cacdafe8
Danner36 <jareddanner3@gmail.com>
2016-03-02 10:47:26 -0600

    import.ir library

--- app/src/main/java/edu/isu/jab/jab_isu/MainActivity.java
+++ app/src/main/java/edu/isu/jab/jab_isu/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.hardware.ConsumerIrManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,4 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
