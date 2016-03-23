28a8b2a47ead3a86cd5aef8a2ffdf7d78eba8585
Danner36 <jareddanner3@gmail.com>
2016-03-23 10:13:21 -0500

    Reverted to old motion method

--- app/src/main/java/edu/isu/jab/jab_isu/MainActivity.java
+++ app/src/main/java/edu/isu/jab/jab_isu/MainActivity.java
@@ -25,11 +25,6 @@ import android.app.Activity;  //NOT USED
 import android.os.Bundle;  //NOT USED
 import android.view.MotionEvent;  //NOT USED
 import android.widget.ViewFlipper;
-import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
 
 
 import android.app.Activity; //NOT USED
@@ -66,70 +61,6 @@ public class MainActivity extends AppCompatActivity {
     private ViewFlipper viewFlipper;
     private float lastX;
 
-    public class OnSwipeTouchListener implements OnTouchListener {
-
-        private final GestureDetector gestureDetector;
-
-        public OnSwipeTouchListener (Context ctx){
-            gestureDetector = new GestureDetector(ctx, new GestureListener());
-        }
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            return gestureDetector.onTouchEvent(event);
-        }
-
-        private final class GestureListener extends SimpleOnGestureListener {
-
-            private static final int SWIPE_THRESHOLD = 100;
-            private static final int SWIPE_VELOCITY_THRESHOLD = 100;
-
-            @Override
-            public boolean onDown(MotionEvent e) {
-                return true;
-            }
-
-            @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                boolean result = false;
-                try {
-                    float diffY = e2.getY() - e1.getY();
-                    float diffX = e2.getX() - e1.getX();
-                    if (Math.abs(diffX) > Math.abs(diffY)) {
-                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                            if (diffX > 0) {
-                                onSwipeRight();
-                            } else {
-                                onSwipeLeft();
-                            }
-                        }
-                        result = true;
-                    }
-                    else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                        if (diffY > 0) {
-                            onSwipeBottom();
-                        } else {
-                            onSwipeTop();
-                        }
-                    }
-                    result = true;
-                } catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-                return result;
-            }
-        }
-        public void onSwipeRight() {
-        }
-        public void onSwipeLeft() {
-        }
-        public void onSwipeTop() {
-        }
-        public void onSwipeBottom() {
-        }
-    }
-
-    /*
     public boolean onTouchEvent(MotionEvent touchevent) {
         switch (touchevent.getAction()) {
             case MotionEvent.ACTION_DOWN:
@@ -175,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
-    */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
