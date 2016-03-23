1d140d4f6155152687354c07f9285b63eee681f3
Danner36 <jareddanner3@gmail.com>
2016-03-23 11:15:11 -0500

    fixed brians pathetic mistake

--- app/src/main/java/edu/isu/jab/jab_isu/MainActivity.java
+++ app/src/main/java/edu/isu/jab/jab_isu/MainActivity.java
@@ -118,18 +118,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ViewFlipper viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
         setContentView(R.layout.activity_main);
-        
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
 
         ConsumerIrManager  mCIR = (ConsumerIrManager)getSystemService(CONSUMER_IR_SERVICE);
         //Log.e(TAG,"mCIR.hasIrEmitter(): " + mCIR.hasIrEmitter());
