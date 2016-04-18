87b5e4ac4f0e19cb617909cdf959beed66bcd279
Danner36 <jareddanner3@gmail.com>
2016-04-17 20:11:07 -0500

    BETA VERSION RELEASED: Added : switcher button and method to tab between pages. Removed : Motion Event method. Removed Herobrine.

--- app/src/main/java/edu/isu/jab/jab_isu/MainActivity.java
+++ app/src/main/java/edu/isu/jab/jab_isu/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.FeatureInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -33,56 +34,11 @@ import android.util.Log;
 import android.widget.ViewFlipper;  //NOT USED
 import java.util.Scanner;
 
+import static android.view.MotionEvent.*;
+
 public class MainActivity extends AppCompatActivity {
     
     private ViewFlipper viewFlipper;
-    private float lastX;
-
-    public boolean onTouchEvent(MotionEvent touchevent) {
-        switch (touchevent.getAction()) {
-            /*case MotionEvent.ACTION_DOWN:
-                lastX = touchevent.getX();
-//                break;*/
-            case MotionEvent.ACTION_SCROLL:
-                float currentX = touchevent.getX();
-
-                // Handling left to right screen swap.
-                if (lastX < currentX) {
-
-                    // If there aren't any other children, just break.
-                    if (viewFlipper.getDisplayedChild() == 0) {
-
-                        // Next screen comes in from left.
-                        viewFlipper.setInAnimation(this, R.anim.slide_in_from_left);
-
-                        // Current screen goes out from right.
-                        viewFlipper.setOutAnimation(this, R.anim.slide_out_to_right);
-                    }
-                    // Display next screen.
-                    viewFlipper.showNext();
-                }
-
-                // Handling right to left screen swap.
-                if (lastX > currentX) {
-
-                    // If there is a child (to the left), kust break.
-                    if (viewFlipper.getDisplayedChild() == 1) {
-
-                        // Next screen comes in from right.
-                        viewFlipper.setInAnimation(this, R.anim.slide_in_from_right);
-
-                        // Current screen goes out from left.
-                        viewFlipper.setOutAnimation(this, R.anim.slide_out_to_left);
-                    }
-                    // Display previous screen.
-                    viewFlipper.showPrevious();
-                }
-                break;
-            default:
-                break;
-        }
-        return false;
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -115,8 +70,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ViewFlipper viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+
         setContentView(R.layout.activity_main);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipperA);
 
         PackageManager pm = getPackageManager();
         FeatureInfo[] fi = pm.getSystemAvailableFeatures();
@@ -270,6 +226,17 @@ public class MainActivity extends AppCompatActivity {
     //HIGH FIVE
     public void onButtonClickHighFive(View V){
         Toast.makeText(MainActivity.this, "HIGHT FIVE!", Toast.LENGTH_SHORT).show();
+    }
+    //SWTICH VIEW
+    public void onButtonClickSwitcher(View V){
+        int i = viewFlipper.getDisplayedChild();
+        if(i == 0 || i == 1){
+            viewFlipper.showNext();
+        }
+        else{
+            viewFlipper.showPrevious();
+            viewFlipper.showPrevious();
+        }
     }
 
     //SECOND TEMPLATE (STATIONARY MOVEMENT)
